83ca83086416419be44233e5c7e1048597a35930
YOUR_NAME <YOUR_EMAIL>
2025-02-12 22:41:17 -0600

    Completed

--- boggle_board.py
+++ boggle_board.py
@@ -2,7 +2,7 @@
 import random
 
 class BoggleBoard:
-  
+  list_of_words = []
   def __init__(self):
     pass
 
@@ -15,27 +15,44 @@ class BoggleBoard:
      print(msg)
   
   def print_random(self):
-    print("".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=4)).lower())
-    print("".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=4)).lower())
-    print("".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=4)).lower())
-    print("".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=4)).lower())
+    word1 = "".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=4)).lower()
+    word2 = "".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=4)).lower()
+    word3 = "".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=4)).lower()
+    word4 = "".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=4)).lower()
+    vertical_word1 = "".join(word1[0] + word2[0] + word3[0] + word4[0])
+    vertical_word2 = "".join(word1[1] + word2[1] + word3[1] + word4[1])
+    vertical_word3 = "".join(word1[2] + word2[2] + word3[2] + word4[2])
+    vertical_word4 = "".join(word1[3] + word2[3] + word3[3] + word4[3])
+    BoggleBoard.list_of_words.extend([word1, word2, word3, word4])
+    BoggleBoard.list_of_words.extend([word1[::-1], word2[::-1], word3[::-1], word4[::-1]])
+    BoggleBoard.list_of_words.extend([vertical_word1, vertical_word2, vertical_word3, vertical_word4])
+    BoggleBoard.list_of_words.extend([vertical_word1[::-1], vertical_word2[::-1], vertical_word3[::-1], vertical_word4[::-1]])
+    print(word1)
+    print(word2)
+    print(word3)
+    print(word4)
 
 
-  def shake(self):
+  def shake(self, word):
     while True:
-      # keep_running = True
       user_input = input("Do you want to shake?: ").lower()
       if user_input == 'quit':
         break
       elif user_input == "yes":
         self.print_random()
+        self.include_word(word)
       elif user_input == 'no':
         self.print_blank()
   
-  def include_word(self):
-      pass
+  def include_word(self, word):
+      if word in BoggleBoard.list_of_words:
+        print(f"{word} has a match!")
+      else:
+        print(f"{word} has no matches. Try again!")
+      
 
 
 first = BoggleBoard()
 # first.print_blank()
-first.shake()
+first.shake('bear')
+first.include_word('bear')
--- testing.py
+++ testing.py
@@ -4,12 +4,21 @@ line1 = 'LKNE'
 line2 = 'QAHH'
 line3 = 'VKPC'
 line4 = 'EVXE'
-
+horizontal_lines = [line1, line2, line3, line4]
 
 h1_reversed = line1[::-1]
-h2_back = line2[::-1]
-h3_back = line3[::-1]
-h4_back = line4[::-1]
+h2_reversed = line2[::-1]
+h3_reversed = line3[::-1]
+h4_reversed = line4[::-1]
 
-print(line1.lower())
-print(h1_reversed)
+v1 = "".join(line1[0] + line2[0] + line3[0] + line4[0])
+v2 = "".join(line1[1] + line2[1] + line3[1] + line4[1])
+v3 = "".join(line1[2] + line2[2] + line3[2] + line4[2])
+v4 = "".join(line1[3] + line2[3] + line3[3] + line4[3])
+print(line1)
+print(line1[::-1])
+print(v1)
+print(v1[::-1])
+
+if 'LKNE' in horizontal_lines:
+    print('yes')
