2029ad93d45ec9fbb44f24d48dc42953244b5fd8
YOUR_NAME <YOUR_EMAIL>
2025-02-12 21:52:50 -0600

    working on boggle board ii

--- boggle_board.py
+++ boggle_board.py
@@ -1,11 +1,41 @@
 # You should re-use and modify your old BoggleBoard class to support the new requirements
+import random
 
 class BoggleBoard:
-	def __init__(self):
-      pass
+  
+  def __init__(self):
+    pass
 
-	def shake(self):
-      pass
+  def print_blank(self):
+     msg = """
+-----
+-----
+-----
+"""
+     print(msg)
+  
+  def print_random(self):
+    print("".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=4)).lower())
+    print("".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=4)).lower())
+    print("".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=4)).lower())
+    print("".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=4)).lower())
+
+
+  def shake(self):
+    while True:
+      # keep_running = True
+      user_input = input("Do you want to shake?: ").lower()
+      if user_input == 'quit':
+        break
+      elif user_input == "yes":
+        self.print_random()
+      elif user_input == 'no':
+        self.print_blank()
   
   def include_word(self):
       pass
+
+
+first = BoggleBoard()
+# first.print_blank()
+first.shake()
---
+++ testing.py
@@ -0,0 +1,15 @@
+
+# using shake() returns:
+line1 = 'LKNE'
+line2 = 'QAHH'
+line3 = 'VKPC'
+line4 = 'EVXE'
+
+
+h1_reversed = line1[::-1]
+h2_back = line2[::-1]
+h3_back = line3[::-1]
+h4_back = line4[::-1]
+
+print(line1.lower())
+print(h1_reversed)
