a2a4a167508dff4b0aed3c5ecfa1fb8b7cc03192
YOUR_NAME <YOUR_EMAIL>
2025-02-12 22:56:14 -0600

    changed the list of all words to a set of all words

--- boggle_board.py
+++ boggle_board.py
@@ -1,8 +1,18 @@
-# You should re-use and modify your old BoggleBoard class to support the new requirements
+"""""
+In this typical case, set lookups would be roughly 100-1000x faster than list lookups. This is because:
+Lists check each element one by one (O(n) time complexity)
+Sets use hash tables for instant lookups (O(1) time complexity)
+For a Boggle board where you're frequently checking if words exist, this difference could mean:
+List: ~0.1-1 milliseconds per word check
+Set: ~0.001-0.01 milliseconds per word check
+While this might not matter for casual play, it would become very noticeable if you were doing something like checking all possible words on the board against a dictionary.
+"""""
+## Added this answer from claude.ai on why a set would be faster/more efficient that using a list
+
 import random
 
 class BoggleBoard:
-  list_of_words = []
+  set_of_words = set()
   def __init__(self):
     pass
 
@@ -23,8 +33,8 @@ class BoggleBoard:
     vertical_word2 = "".join(word1[1] + word2[1] + word3[1] + word4[1])
     vertical_word3 = "".join(word1[2] + word2[2] + word3[2] + word4[2])
     vertical_word4 = "".join(word1[3] + word2[3] + word3[3] + word4[3])
-    BoggleBoard.list_of_words.extend([word1, word2, word3, word4, word1[::-1], word2[::-1], word3[::-1], word4[::-1]])
-    BoggleBoard.list_of_words.extend([vertical_word1, vertical_word2, vertical_word3, vertical_word4, vertical_word1[::-1], vertical_word2[::-1], vertical_word3[::-1], vertical_word4[::-1]])
+    BoggleBoard.set_of_words.update([word1, word2, word3, word4, word1[::-1], word2[::-1], word3[::-1], word4[::-1]])
+    BoggleBoard.set_of_words.update([vertical_word1, vertical_word2, vertical_word3, vertical_word4, vertical_word1[::-1], vertical_word2[::-1], vertical_word3[::-1], vertical_word4[::-1]])
     print(word1)
     print(word2)
     print(word3)
@@ -44,7 +54,7 @@ class BoggleBoard:
   
   def include_word(self, word):
       # print(BoggleBoard.list_of_words)
-      if word in BoggleBoard.list_of_words:
+      if word in BoggleBoard.set_of_words:
         print(f"{word} has a match!")
       else:
         print(f"{word} has no matches. Try again!")
